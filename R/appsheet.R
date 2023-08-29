46c4b5747abf827503da687d455fba0e25434209
Samuel Calderon <samuel.calderon@uarm.pe>
2023-08-29 09:21:03 -0500

    fix check() errors

--- R/appsheet.R
+++ R/appsheet.R
@@ -1,11 +1,13 @@
 #' Appsheet Function
 #'
-#' This function interacts with the AppSheet API to perform actions on a specified table.
+#' This function interacts with the AppSheet API to perform actions on a specified table. 
+#' For more info, check the [official documentation](https://support.google.com/appsheet/answer/10105768?hl=en&ref_topic=10105767&sjid=5773035771729381163-SA).
 #'
 #' @param tableName The name of the table to perform actions on.
-#' @param Action The action to be performed on the table. Default is "Find".
+#' @param Action The action to be performed on the table. Default is "Find", which reads a table.
 #' @param Properties A list of properties for the action. Default is a list with Locale set to "en-US".
 #' @param Rows A list of rows for the action. Default is an empty list.
+#' @param Selector Expression to select and format the rows returned. Only valid when Action is "Find".
 #' @param appId The AppSheet application ID. Default is retrieved from the APPSHEET_APP_ID environment variable.
 #' @param access_key The AppSheet application access key. Default is retrieved from the APPSHEET_APP_ACCESS_KEY environment variable.
 #'
@@ -16,7 +18,10 @@
 #' @importFrom magrittr %>%
 #' 
 #' @examples
-#' app_data <- appsheet("my_table", Action = "Find", Properties = list(Locale = "en-US"))
+#' \dontrun{
+#' appsheet("my_table")
+#' appsheet("my_table", Properties = list(Locale = "en-US"))
+#' }
 #'
 appsheet <- function(
 		tableName,
